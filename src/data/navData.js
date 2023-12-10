3bc625b42ea32f5ce84f02ef105d4d704ddedcba
Sandra Veronica <sandrasarang77@gmail.com>
2023-12-09 23:41:59 -0500

    add components for header, styles and fonts

---
+++ src/data/navData.js
@@ -0,0 +1,52 @@
+const navData = [
+  {
+    name: 'Inicio',
+    path: '/'
+  },
+  {
+    name: 'Dermatología',
+    path: '/tratamientos/dermatología'
+  },
+  {
+    name: 'Medicina Estética',
+    collection: 'tratamientos',
+    sub: [
+      {
+        name: 'Faciales',
+        path: '/tratamientos/'
+      },
+      {
+        name: 'Corporales',
+        path: '/tratamientos/'
+      },
+      {
+        name: 'Láser',
+        path: '/tratamientos/'
+      }
+    ]
+  },
+  {
+    name: 'Oficinas',
+    collection: 'oficinas',
+    sub: [
+      {
+        name: 'José Luis Bustamante y Rivero',
+        path: '/oficinas/'
+      },
+      {
+        name: 'León Velarde 311',
+        path: '/oficinas/'
+      },
+      {
+        name: 'Yanahuara',
+        path: '/oficinas/'
+      }
+    ]
+  },
+  {
+    name: 'Contactos',
+    path: '/contactos/'
+  }
+]
+
+export default navData
